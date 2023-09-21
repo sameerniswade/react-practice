3bc61fff82aa4a799c72751497e2c3a24d810e68
sameer <38357131+sameerniswade@users.noreply.github.com>
2023-09-21 12:17:33 +0000

    made a counter using react

--- .devcontainer/reactPractice/src/App.jsx
+++ .devcontainer/reactPractice/src/App.jsx
@@ -1,9 +1,32 @@
 import { useState } from "react";
 
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
-  return <div>hello world</div>;
+  const [counter, setCounter] = useState(0);
+  const increaseCount = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+  const decreaseCount = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <h1>count : {counter}</h1>
+      <div className={styles.buttons}>
+        <button onClick={increaseCount}>
+          click here to Increase count by 1
+        </button>
+        <button onClick={decreaseCount}>
+          click here to decrease count by 1
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
